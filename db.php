e5e1859fc2ace20a796093f71852a8d145f89858
Theekshana <145346863+ThikkaBro@users.noreply.github.com>
2025-11-22 19:53:31 +0530

    updated DB V1.1

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "127.0.0.1";
-$user = "root"; // Update this
-$pass = ""; // Update this
-$db   = "quinn_blog";   // Update this
+$host = "localhost";
+$user = "magaqmco_quinn"; // Update this
+$pass = "Quinn@2025"; // Update this
+$db   = "magaqmco_quinn_blog";   // Update this
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
