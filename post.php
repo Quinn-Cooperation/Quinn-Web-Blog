71193cd8788559295288e60a04fa5c72ad805d7b
Theekshana <145346863+ThikkaBro@users.noreply.github.com>
2025-11-28 12:24:26 +0530

    Final

--- post.php
+++ post.php
@@ -28,8 +28,7 @@ include 'header.php';
             <div class="col-lg-10">
                 <div class="mil-image-frame mil-horizontal mil-up mil-mb-60">
                     <img src="<?php echo $post['image_url']; ?>" 
-                         alt="Cover Image" 
-                         style="width: 1080px; height: 1080px; object-fit: contain; background-color: #000;">
+                         alt="Cover Image">
                 </div>
                 <div class="mil-text-lg mil-up mil-mb-60">
                     <?php echo $post['content']; ?>
