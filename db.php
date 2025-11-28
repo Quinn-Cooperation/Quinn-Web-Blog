21ce8429a4cfafd96a9f20bc4789e2266ea411d6
Theekshana <145346863+ThikkaBro@users.noreply.github.com>
2025-11-28 12:33:20 +0530

    ok

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "magaqmco_quinn"; // Update this
-$pass = "Quinn@2025"; // Update this
-$db   = "magaqmco_quinn_blog";   // Update this
-
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-?>
