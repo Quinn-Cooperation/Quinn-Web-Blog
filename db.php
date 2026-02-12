aa2e08be4b2f6159a7dba801729c7b1d9ea6d1c7
Theekshana <145346863+ThikkaBro@users.noreply.github.com>
2026-02-12 06:45:33 +0530

    Updated DB

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "magaqmco_quinn"; // Update this
+$user = "buwaggif_quinnblog"; // Update this
 $pass = "Quinn@2025"; // Update this
-$db   = "magaqmco_quinn_blog";   // Update this
+$db   = "buwaggif_quinn_blog";   // Update this
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
